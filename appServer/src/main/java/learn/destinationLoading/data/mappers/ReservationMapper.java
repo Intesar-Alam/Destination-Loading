4c98459e8b9cb685a83e37e25985b7cc18a461a8
jsnreedie <reediejason@gmail.com>
2022-06-25 17:42:00 -0400

    changed field name to userAccountId to reflect db name, added missing CompanyId field to reservation, and completed ReservationMapper

--- appServer/src/main/java/learn/destinationLoading/data/mappers/ReservationMapper.java
+++ appServer/src/main/java/learn/destinationLoading/data/mappers/ReservationMapper.java
@@ -1,4 +1,22 @@
 package learn.destinationLoading.data.mappers;
 
-public class ReservationMapper {
+import learn.destinationLoading.models.Reservation;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class ReservationMapper implements RowMapper<Reservation> {
+
+    @Override
+    public Reservation mapRow(ResultSet rs, int i) throws SQLException {
+        Reservation reservation = new Reservation();
+        reservation.setReservationId(rs.getInt("reservation_id"));
+        reservation.setUserAccountId(rs.getInt("user_account_id"));
+        reservation.setCompanyId(rs.getInt("company_id"));
+        reservation.setReservationDate(rs.getDate("reservation_date").toLocalDate());
+        reservation.setReservationCode(rs.getString("reservation_code"));
+
+        return reservation;
+    }
 }
