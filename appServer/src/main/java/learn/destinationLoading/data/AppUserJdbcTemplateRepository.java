45a7c5c0457f39909e06a52f9f14546904e5481b
jsnreedie <reediejason@gmail.com>
2022-06-25 18:28:12 -0400

    added app_user_id as fk to user_account table in db to associate users with roles

--- appServer/src/main/java/learn/destinationLoading/data/AppUserJdbcTemplateRepository.java
+++ appServer/src/main/java/learn/destinationLoading/data/AppUserJdbcTemplateRepository.java
@@ -1,4 +1,20 @@
 package learn.destinationLoading.data;
 
-public class AppUserJdbcTemplateRepository {
+import learn.destinationLoading.models.AppUser;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.transaction.annotation.Transactional;
+
+public class AppUserJdbcTemplateRepository implements AppUserRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public AppUserJdbcTemplateRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    @Transactional
+    public AppUser findByUsername(String username) {
+        
+    }
 }
