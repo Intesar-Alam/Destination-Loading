211a0b51887e7c431fef37ac6e47b8678392ee4e
akhanson27 <a.k.hanson27@gmail.com>
2022-06-28 13:55:20 -0500

    #64 add Not Found styling

--- react-client-type/src/components/NotFound.tsx
+++ react-client-type/src/components/NotFound.tsx
@@ -1,6 +1,27 @@
+
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+
+import JumboImage from '../images/travelers.jpg';
+
 function NotFound() {
   return (
-    <h2>Not Found</h2>
+    <>
+      <Card className="text-white rounded-0 col-md-10 mx-auto my-5">
+        <Card.Img src={JumboImage} alt="Card image" className="rounded-0" />
+        <Card.ImgOverlay>
+          <Card.Title style={{ fontSize: "36px" }}>Error</Card.Title>
+          <Card.Text style={{ fontSize: "100px" }}>
+            404
+          </Card.Text>
+          <Card.Text className="text-dark">
+            There seems to be an error, we don't want you to miss your trip please go to our <Button>Home</Button> or <Button className="float-right">Contact Us</Button> for more information.
+          </Card.Text>
+          
+          
+        </Card.ImgOverlay>
+      </Card>
+    </>
   );
 }
 
