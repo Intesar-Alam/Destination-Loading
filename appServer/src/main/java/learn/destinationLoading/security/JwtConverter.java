9aea15470070dfcb6bd15aab56d5abf21ec647b5
jsnreedie <reediejason@gmail.com>
2022-06-29 14:22:53 -0400

    #70 partial jwt converter

--- appServer/src/main/java/learn/destinationLoading/security/JwtConverter.java
+++ appServer/src/main/java/learn/destinationLoading/security/JwtConverter.java
@@ -1,7 +1,35 @@
 package learn.destinationLoading.security;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.stream.Collectors;
 
 @Component
 public class JwtConverter {
+
+    private Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+
+    private final String ISSUER = "destination-loading-api";
+    private final int EXPIRATION_MINUTES = 480;
+    private final int EXPIRATION_MILLIS = EXPIRATION_MINUTES * 60 * 1000;
+
+    public String getTokenFromUser(User user) {
+        String authorities = user.getAuthorities().stream()
+                .map(i -> i.getAuthority())
+                .collect(Collectors.joining(","));
+
+        return Jwts.builder() // the builder pattern
+                .setIssuer(ISSUER)
+                .setSubject(user.getUsername())
+                .claim("authorities", authorities)
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_MILLIS))
+                .signWith(key)
+                .compact();
+    }
 }
