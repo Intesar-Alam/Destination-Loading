3b85378066b57d15b6bdead3eb89cd7c094fdd4a
akhanson27 <a.k.hanson27@gmail.com>
2022-06-30 10:42:45 -0500

    #46 add handlechange

--- react-client-type/src/components/CompanyForm.tsx
+++ react-client-type/src/components/CompanyForm.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
+import { useNavigate, useParams, Link } from 'react-router-dom';
 
 
 import Form from 'react-bootstrap/Form';
@@ -9,15 +9,20 @@ import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Card from 'react-bootstrap/Card';
 
-const COMPANY_DEFAULT = {
-  companyName: '',
-  url: '',
-  icon: '',
-  transportationMode: ''
+type COMPANY_DEFAULT = {
+  companyName: string,
+  url: string,
+  icon: string,
+  transportationMode: string
 };
 
 function CompanyForm() {
-  const [company, setCompany] = useState(COMPANY_DEFAULT);
+  const [company, setCompany] = useState<COMPANY_DEFAULT>({
+    companyName: "",
+    url: "",
+    icon: "",
+    transportationMode: "",
+  });
   const [errors, setErrors] = useState([]);
 
   const navigate = useNavigate();
@@ -40,8 +45,9 @@ function CompanyForm() {
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
 
-    setCompany('');
+    setCompany({...company,[event.target.name] : event.target.value});
   };
+
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -91,27 +97,27 @@ function CompanyForm() {
         <Card className="rounded-0 col-md-8 mx-auto">
           <Form onSubmit={handleSubmit}>
             <Form.Group as={Row} className="my-2 ms-3" controlId="formCompanyName">
-              <Form.Label column sm={2} htmlFor="companyName">Company Name</Form.Label>
+              <Form.Label column sm={2}>Company Name</Form.Label>
               <Col sm={9}>
-                <Form.Control id="companyName" type="text" placeholder="Enter Company Name" onChange={handleChange} />
+                <Form.Control type="text" placeholder="Enter Company Name" value={company['companyName']} onChange={handleChange} />
               </Col>
             </Form.Group>
             <Form.Group as={Row} className="mb-2 ms-3" controlId="formCompanyUrl">
-              <Form.Label column sm={2} htmlFor="url">Company URL/Website</Form.Label>
+              <Form.Label column sm={2}>Company URL/Website</Form.Label>
               <Col sm={9}>
-                <Form.Control id="url" type="text" placeholder="Enter Company URL" onChange={handleChange} />
+                <Form.Control type="text" placeholder="Enter Company URL" value={company['url']} onChange={handleChange} />
               </Col>
             </Form.Group>
             <Form.Group as={Row} className="mb-2 ms-3" controlId="formIcon">
-              <Form.Label column sm={2} htmlFor="icon">Company Icon</Form.Label>
+              <Form.Label column sm={2}>Company Icon</Form.Label>
               <Col sm={9}>
-                <Form.Control id="icon" type="text" placeholder="icon" onChange={handleChange} />
+                <Form.Control type="text" placeholder="icon" value={company['icon']} onChange={handleChange} />
               </Col>
             </Form.Group>
-            <Form.Group as={Row} className="mb-2 ms-3" controlId="formTransportationMode">
-              <Form.Label column sm={2} htmlFor="transportationMode">Transport Mode</Form.Label>
+            {/* <Form.Group as={Row} className="mb-2 ms-3" controlId="formTransportationMode">
+              <Form.Label column sm={2}>Transport Mode</Form.Label>
               <Col sm={9}>
-                <Form.Select id="transportationMode" aria-label="Select Transportation Mode" onChange={handleChange}>
+                <Form.Select aria-label="Select Transportation Mode" value={company['transportationMode']} onChange={handleChange}>
                   <option>Select One</option>
                   <option value="1">AIR</option>
                   <option value="2">RAIL</option>
@@ -119,24 +125,24 @@ function CompanyForm() {
                   <option value="4">WATER</option>
                 </Form.Select>
               </Col>
-            </Form.Group>
+            </Form.Group> */}
             <hr style={{ color: 'gray', height: '1px' }} />
-            <Form.Group as={Row} className="mb-2 ms-3" controlId="formRepIdSelect">
-              <Form.Label column sm={2} htmlFor="repIdSelect">Company Rep</Form.Label>
+            {/* <Form.Group as={Row} className="mb-2 ms-3" controlId="formRepIdSelect">
+              <Form.Label column sm={2}>Company Rep</Form.Label>
               <Col sm={9}>
-                <Form.Select id="repIdSelect" aria-label="Select Company Representative" onChange={handleChange}>
+                <Form.Select aria-label="Select Company Representative" value={company['repId']} onChange={handleChange}>
                   <option>Select One</option>
-                  {/* TODO add map/fetch for rep users */}
+                  // TODO add map/fetch for rep users 
                   <option>AIR</option>
                 </Form.Select>
-              </Col>
-            </Form.Group>
+              </Col> */}
+            {/* </Form.Group> */}
             {/* TODO add conditional rendering for create/edit(update) user */}
             <Form.Group className="mb-3">
               <Row>
                 <Col md={{ span: 5, offset: 7 }}>
                   <Button type="submit" className="me-2">Create Company</Button>
-                  <Button type="submit">Cancel</Button>
+                  <Link className="btn btn-primary" to="/companylist">Cancel</Link>
                 </Col>
               </Row>
             </Form.Group>
