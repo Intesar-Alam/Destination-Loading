b1be2ceb8b418bce3778afe6c5ad087ca86431ca
akhanson27 <a.k.hanson27@gmail.com>
2022-06-30 14:36:03 -0500

    #43 updates to form

--- react-client-type/src/components/ReservationForm.tsx
+++ react-client-type/src/components/ReservationForm.tsx
@@ -17,37 +17,37 @@ function ReservationForm() {
         <Card className="rounded-0 col-md-8 mx-auto">
           <Form>
             <Form.Group as={Row} className="my-2 ms-3" controlId="formReservationTitle">
-              <Form.Label column sm={2} htmlFor="reservationTitle">Trip Title</Form.Label>
+              <Form.Label column sm={2}>Trip Title</Form.Label>
               {/* max 40 characters */}
               <Col sm={9}>
-                <Form.Control id="reservationTitle" type="text" placeholder="Enter trip title or short description" />
-                <Form.Text id="reservationTitle" muted>
+                <Form.Control type="text" placeholder="Enter trip title or short description" />
+                <Form.Text muted>
                   Maximum 40 characters.
                 </Form.Text>
               </Col>
             </Form.Group>
             <Form.Group as={Row} className="mb-3 ms-3" controlId="formReservationDate">
-              <Form.Label column sm={2} htmlFor="reservationDate">Trip Date</Form.Label>
+              <Form.Label column sm={2}>Trip Date</Form.Label>
               <Col sm={5}>
-                <Form.Control id="reservationDate" type="date" />
-                <Form.Text id="reservationDate" muted>
+                <Form.Control type="date" />
+                <Form.Text muted>
                   Select trip start date.
                 </Form.Text>
               </Col>
             </Form.Group>
             <Form.Group as={Row} className="mb-2 ms-3" controlId="formReservationCode">
-              <Form.Label column sm={2} htmlFor="reservationCode">Reservation Code</Form.Label>
+              <Form.Label column sm={2}>Reservation Code</Form.Label>
               <Col sm={9}>
                 <InputGroup>
-                <Form.Control id="reservationCode" type="text" placeholder="Enter Reservation Code" />
-                <Button variant="outline-secondary" id="copyButton">Copy</Button>
+                <Form.Control type="text" placeholder="Enter Reservation Code" />
+                <Button variant="outline-secondary">Copy</Button>
                 </InputGroup>
               </Col>
             </Form.Group>
             <Form.Group as={Row} className="mb-2 ms-3" controlId="formCompanySelector">
-              <Form.Label column sm={2} htmlFor="companySelector">Company</Form.Label>
+              <Form.Label column sm={2}>Company</Form.Label>
               <Col sm={9}>
-                <Form.Select id="companySelector" aria-label="Select Company">
+                <Form.Select aria-label="Select Company">
                   <option>Select One</option>
                   {/* Map over company data to populate dropdown list */}
                   <option>CompanyNamesHere</option>
