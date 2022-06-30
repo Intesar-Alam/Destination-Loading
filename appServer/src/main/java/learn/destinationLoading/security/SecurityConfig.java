d4b1b58bcef23bb9f3fb563f8631abf491f137d8
jsnreedie <reediejason@gmail.com>
2022-06-30 12:32:15 -0400

    #70 finished authcontroller

--- appServer/src/main/java/learn/destinationLoading/security/SecurityConfig.java
+++ appServer/src/main/java/learn/destinationLoading/security/SecurityConfig.java
@@ -1,4 +1,26 @@
 package learn.destinationLoading.security;
-//@EnableWebSecurity
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
 public class SecurityConfig {
+
+    private final JwtConverter jwtConverter;
+
+    public SecurityConfig(JwtConverter jwtConverter) {
+        this.jwtConverter = jwtConverter;
+    }
+
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//
+//        http.csrf().disable();
+//
+//        http.cors();
+//
+//        http.authorizeRequests()
+//                .antMatchers()
+//    }
+
 }
