aad25f441aff5b2ee7c0d1abfc9bc53eecdf9d35
Intesar Alam <ialam5539@bths.edu>
2022-06-30 15:51:48 -0400

    85 all reservations list done.

--- react-client-type/src/components/ReservationList.tsx
+++ react-client-type/src/components/ReservationList.tsx
@@ -1,11 +1,59 @@
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+
 import Table from 'react-bootstrap/Table';
 import Button from 'react-bootstrap/Button';
-
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function ReservationList() {
+  const [reservations, setReservations] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:8080/test/reservation')
+      .then(response => {
+        if (response.status === 200) {
+          return response.json();
+        } else {
+          return Promise.reject(`Unexpected status code: ${response.status}`);
+        }
+      })
+      .then(data => setReservations(data))
+      .catch(console.log);
+  }, []);
+
+
   return (
     <>
       <h1>All Reservations</h1>
+      <Container>
+
+        <Table>
+          <thead className="thead-dark">
+            <tr>
+              <th>Reservation ID</th>
+              <th>Customer ID</th>
+              <th>Company ID</th>
+              <th>Reservation Date</th>
+              <th>Reservation Code</th>
+              <th>Reservation Name</th>
+            </tr>
+          </thead>
+          <tbody>
+            {reservations.map(reservation => (
+              <tr key={reservation['reservationId']}>
+                <td>{reservation['reservationId']}</td>
+                <td>{reservation['appUserId']}</td>
+                <td>{reservation['companyId']}</td>
+                <td>{reservation['reservationDate']}</td>
+                <td>{reservation['reservationCode']}</td>
+                <td>{reservation['reservationTitle']}</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </Container>
       {/* <Table>
         <thead className="thead-dark">
           <tr>
