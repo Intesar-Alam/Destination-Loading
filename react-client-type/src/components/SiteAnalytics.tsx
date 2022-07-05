e3632018a1085b18f5dc286182d5f97d36d16f82
akhanson27 <a.k.hanson27@gmail.com>
2022-07-05 18:52:47 -0500

    #64 add underconstruction page

--- react-client-type/src/components/SiteAnalytics.tsx
+++ react-client-type/src/components/SiteAnalytics.tsx
@@ -1,7 +1,7 @@
 
 import Card from 'react-bootstrap/Card';
 
-import JumboImage from '../images/travelers.jpg';
+import JumboImage from '../images/trainconstruction.jpg';
 
 import AdminMenuBar from './AdminMenuBar';
 
@@ -10,11 +10,15 @@ function SiteAnalytics() {
   return (
     <>
     <AdminMenuBar />
-      <Card className="text-white rounded-0 col-md-10 mx-auto my-5">
+      <Card className="text-black rounded-0 col-md-10 mx-auto my-5">
         <Card.Img src={JumboImage} alt="Card image" className="rounded-0" />
-        <Card.ImgOverlay>
-          <Card.Title className="text-center" style={{background: 'rgba(0,0,0, 0.55)', fontSize: '48px' }}>New Features Coming Soon!!</Card.Title>
-          <Card.Text className="text-center" style={{background: 'rgba(0,0,0, 0.55)', fontSize: '48px'}}>Checkback later for our latest improvements!</Card.Text>
+        <Card.ImgOverlay className="mx-auto my-auto rounded-0" style={{ backgroundColor: 'rgba(255, 255, 255, 0.5)', width: '96%', height: '95%' }}>
+          <Card.Body className="text-center">
+            <Card.Title style={{ fontSize: '5rem' }}>New Features Coming Soon!!</Card.Title>
+            <Card.Text style={{ fontSize: '3rem' }}>
+            Checkback later for our latest improvements!
+            </Card.Text>
+          </Card.Body>
         </Card.ImgOverlay>
       </Card>
     </>
